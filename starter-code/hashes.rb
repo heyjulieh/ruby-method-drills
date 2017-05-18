d8c89932ad7361d06bc9d14b64fc99a2dfd7fc2c
heyjulieh <heyjulieh@gmail.com>
2017-05-18 16:26:33 -0700

    worked some more on this during class

--- starter-code/hashes.rb
+++ starter-code/hashes.rb
@@ -7,6 +7,18 @@
   # ignores case
   # returns the hash
 
+  def character_count word
+    word.downcase!
+  word_array = word.split("")
+  counts = {}
+  word_array.each do |letter|
+    counts[letter] = 0 unless counts.include?(letter)
+    counts[letter] += 1
+    end
+    counts
+  end
+
+
 ## STRETCH ##
 #word_count
   # takes in a string
@@ -14,6 +26,17 @@
   # ignores case
   # ignores characters that are not in the sequence a-z
   # returns a hash with all the words and their counts
+
+  def word_count (word)
+      word_no_wcharac = word.downcase.gsub(/[^a-z\s]/i, '')
+      word_array = word_no_wcharac.split(" ")
+      counts = {}
+      word_array.each do |word|
+      counts[word] = 0 unless counts.include?(word)
+      counts[word] += 1
+      end
+      counts
+    end
 
 ## STRETCH ##
 #most_frequent_word
--- starter-code/numbers.rb
+++ starter-code/numbers.rb
@@ -6,6 +6,15 @@
   # returns an array containing every integer from 0 to n
   # counts up or down
   # rounds off decimals
+  def count_to (n)
+    n = n.to_i
+      if n >=0
+      0.upto(n).to_a
+      else
+      0.downto(n).to_a
+      end
+  end
+
 
 #is_integer?
   # takes in a number
@@ -15,6 +24,12 @@
   # returns false for Float::NAN
   # returns false for non-numbers
 
+  def is_integer?(n)
+    n.class == Bignum || n.class == Fixnum ||
+      ( n.is_a?(Float) && !n.nan? && n.to_i == n )
+  end
+
+
 #is_prime?
   # takes in a number and checks if it's prime
   # returns false for non-integer decimals
@@ -22,12 +37,22 @@
   # returns false for numbers divisible by anything but 1 and themselves
   # returns true for prime numbers
   # Hint: google prime numbers!
+  def is_prime (n)
+    for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
+  true
+ end
 
 #primes_less_than
   # takes in a number
   # returns an empty array if there are no primes below num
   # does not return the number itself
   # finds all primes less than the given number
+  def primes_less_than (n)
+  end
 
 ## STRETCH ##
 #iterative_factorial
@@ -36,3 +61,5 @@
   # returns NaN for numbers less than 0
   # returns NaN for non-integers
   # calculates and returns the factorial of the input number
+  def iteractive_factorial (n)
+  end
